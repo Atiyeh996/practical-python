82129d9de7a81dadf1343c8e129bfd1f968c32be
Atiyeh996 <atahavorgar@mun.ca>
2021-05-22 04:00:05 +0430

    Solution for exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+Total_amount = 0
+
+for line in f :
+   row = line.split(',')
+   Total_amount = Total_amount + float(row[2])  * int(row[1])
+print("Total amount is", Total_amount)
