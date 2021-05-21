3ffc08ea1fbb453d7500cc4362bbe6a93198104e
Atiyeh996 <atahavorgar@mun.ca>
2021-05-21 22:18:04 +0430

    Solution for exercise 1.10

---
+++ Work/making_a_table.py
@@ -0,0 +1,28 @@
+# exercise 1.10
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months= months + 1
+
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(months , round(total_paid, 2) , round(principal, 2))
+    
+print(f'Total paid${total_paid:.2f}')
+print('Months', months)
+
+
+
