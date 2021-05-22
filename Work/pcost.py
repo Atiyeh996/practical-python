8fded49e0b551d1d230996842b3a19e796d6fe8c
Atiyeh996 <atahavorgar@mun.ca>
2021-05-22 21:03:17 +0430

    Solution for exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,23 @@
 # pcost.py
 #
-# Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
-Total_amount = 0
+# Exercise 1.30
 
-for line in f :
-   row = line.split(',')
-   Total_amount = Total_amount + float(row[2])  * int(row[1])
-print("Total amount is", Total_amount)
+#Total_amount = 0
+
+def portfolio_cost(filename):
+
+   Total_amount = 0
+
+   f = open('Data/portfolio.csv', 'rt')
+   headers = next(f).split(',')
+   #Total_amount = 0
+
+   for line in f :
+      row = line.split(',')
+      Total_amount = Total_amount + float(row[2])  * int(row[1])
+
+   print("Total amount is", Total_amount)
+
+   #return Total_amount
+
 
