86f0143b37dd0e2343e4db6052ee5617757cefdb
Atiyeh996 <atahavorgar@mun.ca>
2021-05-26 03:38:17 +0430

    solution to exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -5,21 +5,31 @@
 import csv
 
 def read_portfolio(filename):
-   
+
+
+   portfolio = {}
    a =[]
    #f = open(filename, 'rt')
    with open(filename, 'rt') as f:
-      #rows = csv.reader(f)
-      headers = next(f).split(',')
+      rows = csv.reader(f)
+      headers = next(rows)
       
    
       for line in f:
-        row = line.split(',')
-        portfolio = (row[0], int(row[1]), float(row[2]))
-        a.append(portfolio)
+
+         row = line.split(',')
+         
+         #portfolio = ("name": row[0], "shares":int(row[1]), "price":float(row[2]))
+         portfolio = {
+            'name'   : row[0],
+            'shares' : int(row[1]),
+            'price'   : float(row[2])
+        }
+         a.append(portfolio)
       #print(a, end='')
-      print(a)
-   return(a)
+   print(a)
+   return a 
 
    #f.close()  
-read_portfolio('Data/portfolio.csv')  
+read_portfolio('Data/portfolio.csv')
+
