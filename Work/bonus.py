5b0c941ade0487374147effc4ff8b5770623ed3b
Atiyeh996 <atahavorgar@mun.ca>
2021-05-26 03:57:49 +0430

    solution for exercise 1.11

--- Work/bonus.py
+++ Work/bonus.py
@@ -4,16 +4,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-month = 0
+months = 0
 extra_payment = 1000.0
 end_duration = 310
 
 while principal > 0:
-    month = month +1
+    months = months +1
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
-    if month == 1 + end_duration:
+    if months == 1 + end_duration:
        principal = principal - extra_payment
 
        total_paid = total_paid + extra_payment 
