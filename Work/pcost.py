c2bd39c92ded937e35d7bc8ade7cede1928c47c3
Atiyeh996 <atahavorgar@mun.ca>
2021-05-26 03:55:17 +0430

    solution for exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -23,7 +23,7 @@ def portfolio_cost(filename):
          try:
             all_shares_price +=int(shares_num) * float(stock_price)
          except ValueError:
-            "we can raise a warning instead using raise warning[msg]"
+            
             print('Missing field encountered, skipping the corresponding line')
 
    return all_shares_price
@@ -32,28 +32,6 @@ if len(sys.argv) ==2:
    filename = sys.argv[1]
 else:
    filename = 'Data/portfolio.csv'
-cost = portfolio_cost(filename)
-print("total cost:" , cost)
+Cost = portfolio_cost(filename)
+print("total cost:" , Cost)
 
-'''
-   try:
-      f = open(filename)
-   except FileNotFoundError:
-      print(f'{filename} is not a valid file')
-      return
-
-   Total_amount = 0
-
-   f = open('Data/portfolio.csv', 'rt')
-   headers = next(f).split(',')
-   #Total_amount = 0
-
-   for line in f :
-      row = line.split(',')
-      Total_amount = Total_amount + float(row[2])  * int(row[1])
-
-   print("Total amount is", Total_amount)
-
-   #return Total_amount
-
-'''
