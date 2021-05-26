d616d4f9c6e619da8cc313b3bad2649eb6007da5
Atiyeh996 <atahavorgar@mun.ca>
2021-05-26 22:22:11 +0430

    solution to exercise2.7

--- Work/report.py
+++ Work/report.py
@@ -4,14 +4,36 @@
 
 import csv 
 
+a=[]
+def read_portfolio(filename):
+
+    with open(filename, 'rt') as f:
+        portfolio={}
+        a=[]
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            portfolio = {
+                "name" : row[0],
+                "shares" : int(row[1]),
+                "price" : float(row[2])
+            }
+            a.append(portfolio)
+        print(a)
+        return a
+a = read_portfolio('Data/portfolio.csv')
+
+
+
 def read_prices(filename):
     
     dic = {}
-    with open(filename) as f:
+    with open(filename, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
             try:
                 dic[row[0]] = float(row[1])
+                #print(dic)
             except IndexError:
                 pass
     
@@ -19,4 +41,20 @@ def read_prices(filename):
     return dic
 
 
-read_prices('Data/prices.csv')
+dict = read_prices('Data/prices.csv')
+
+cost = 0
+for s in a :
+    cost += s["shares"]*s["price"]
+
+print('Total cost', cost)
+
+
+value = 0.0
+for s in dict :
+    value += s['shares']*dict[s['name']]
+    #value += int(s[1])*dict[s[0]]
+
+
+print("total_value" , value)
+print('Gain=', value - cost )
