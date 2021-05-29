a5b57e819a0c55582d0cc28e7a20f0f5bd8b9016
Atiyeh996 <atahavorgar@mun.ca>
2021-05-30 03:48:39 +0430

    exercise 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,27 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+types = []
+def parse_csv(filename, select=None):
+    
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        indices = []
+        record =()
+        records = []
+        for row in rows:
+            if not row:    
+                continue
+            
+            if indices:
+                row = ( row[index] for index in indices )
+
+            if types:
+                row = [func(val) for func, val in zip(types, row) ]
+            
+            record = ()
+            records.append(record)
+
+    return records
+        
+#parse = parse_csv('Data/portfolio.csv', select=['name', 'shares'], types = [str,int])
