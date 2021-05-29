48c6ad15d2bfba5621ef2417b91b4baa02feb146
Atiyeh996 <atahavorgar@mun.ca>
2021-05-30 02:38:30 +0430

    solution for exercise 3.1

--- Work/report.py
+++ Work/report.py
@@ -1,78 +1,49 @@
-
-# report.py
-
-
-import csv 
-
-
-a=[]
-dict = {}
+import csv
 def read_portfolio(filename):
-
     
-    with open(filename, 'rt') as f:
+    portfolio = []
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
+
         for row in rows:
-            portfolio = {
-                "name" : row[0],
-                "shares" : int(row[1]),
-                "price" : float(row[2])
+            record = dict(zip(headers, row))
+            stock = {
+                'name' : record['name'],
+                'shares' : int(record['shares']),
+                'price' : float(record['price'])
             }
-            a.append(portfolio)
-
-        
-    return a
-
+            portfolio.append(stock)
+    return portfolio
 
 def read_prices(filename):
     
-    
-    with open(filename, 'rt') as f:
+    prices = {}
+    with open(filename) as f:
         rows = csv.reader(f)
-        headers = next(rows)
         for row in rows:
-            try: 
-                #dict = print(row)
-                dict[row[0]] = float(row[1])
+            try:
+                prices[row[0]] = float(row[1])
             except IndexError:
                 pass
+
+    return prices
+
+def make_report_data(portfolio,prices):
     
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+
+
+def print_report(reportdata):
     
-    return dict
-
-
-def make_report(filename):
-    list=[]
-
-    for portfolio in a :
-        current_price = a[portfolio['name']]
-        change = current_price - portfolio['price']
-
-        price = dict[portfolio['name']]
-        change = price - portfolio['price']
-        summary = (portfolio['name'], portfolio['shares'], current_price, change)
-        list.append(summary)
-    return list
-
-
-
-def print_report(x,y):
-    headers = ('Name', 'Shares', 'Price', 'Change')
+    headers = ('Name','Shares','Price','Change')
     print('%10s %10s %10s %10s' % headers)
-    print(('-' * 10 + ' ') * len(headers))
-    for row in print_report:
+    print(('-'*10 + ' ')*len(headers))
+    for row in reportdata:
         print('%10s %10d %10.2f %10.2f' % row)
-
-
-def portfolio_report(portfolio_filename, prices_filename):
-
-
-    a = read_portfolio('Data/portfolio.csv')
-    dict = read_prices('Data/prices.csv')
-    report = make_report(a,dict)
-    print_report(report)
-
-portfolio_report(portfolio_filename ='Data/portfolio.csv',prices_filename = 'Data/prices.csv')
-
-
