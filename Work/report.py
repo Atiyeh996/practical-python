f31391fd6ac7989f11b754af0f0156f77ac065ec
Atiyeh996 <atahavorgar@mun.ca>
2021-05-31 18:59:45 +0430

    exercise 3.18

--- Work/report.py
+++ Work/report.py
@@ -1,44 +1,17 @@
 # report.py
-import csv
 
-#import fileparse
+import fileparse
 
 def read_portfolio(filename):
     
-    portfolio = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            record = dict(zip(headers, row))
-            stock = {
-                'name' : record['name'],
-                'shares' : int(record['shares']),
-                'price' : float(record['price'])
-            }
-        
-            portfolio.append(stock)
-
-    return portfolio
-   # return fileparse.parse_csv(filename, select=['name','shares','price'], types=[str,int,float], has_header=True)
-
+    with open(filename) as lines:
+        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
-    
-    prices = {}
-    with open(filename) as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                prices[row[0]] = float(row[1])
-            except IndexError:
-                pass
-
-    return prices
-
-   #
-    
-   #  return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
+   
+   
+    with open(filename) as lines:
+        return dict(fileparse.parse_csv(lines, types=[str,float], has_headers=False))
 
 def make_report_data(portfolio,prices):
     
@@ -57,31 +30,22 @@ def print_report(reportdata):
     print(('-'*10 + ' ')*len(headers))
     for row in reportdata:
         print('%10s %10d %10.2f %10.2f' % row)
-'''
-def portfolio_report(portfoliofile,pricefile):        
-   
 
+def portfolio_report(portfoliofile, pricefile):        
+    
     portfolio = read_portfolio(portfoliofile)
     prices = read_prices(pricefile)
 
-    
-    report = make_report_data(portfolio,prices)
+   
+    report = make_report_data(portfolio, prices)
 
-    
     print_report(report)
 
-portfolio_report('Data/portfolio.csv','Data/prices.csv')
-'''
+def main(args):
+    if len(args) != 3:
+        raise SystemExit('Usage: %s portfile pricefile' % args[0])
+    portfolio_report(args[1], args[2])
 
-def main(argv):
-    portfolio = read_portfolio('Data/portfolio.csv')
-
-    prices = read_prices('Data/prices.csv')
-
-    
-    report = make_report_data(portfolio,prices)
-
-
-if __name__=="__main__":
+if __name__ == '__main__':
     import sys
     main(sys.argv)
